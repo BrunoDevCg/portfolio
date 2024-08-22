94b81c102eb473ff17cd62506692f1a3e6ba5067
ybruno <ybrunodevcg@gmail.com>
2024-08-22 03:38:40 -0400

    alterado cores do contato

--- js/index.js
+++ js/index.js
@@ -149,3 +149,17 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 });
+
+
+
+ScrollReveal({ 
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+
+
+});
+
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
